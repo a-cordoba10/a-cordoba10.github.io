f797f7bdc96e61ba9b3d28976e95f29fcd3a5d72
a-cordoba10 <icoltus@hotmail.com>
2017-08-30 07:55:34 -0500

    Illustrations added

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
  window.onload = function () {
      var images = [{
          'name': 'Study, Work & Die',
-         'path': 'src/collage1.jpg',
+         'path': 'src/ilustracion1.jpg',
          'date': '28/07/2015'
      }, {
-         'name': 'Full of shit,
-         'path': 'src/collage1.jpg',
+         'name': 'Full of shit',
+         'path': 'src/ilustracion2.jpg',
          'date': '05/08/2016'
 }, {
          'name': 'Bear me',
-         'path': 'src/collage1.jpg',
+         'path': 'src/collage2.jpg',
          'date': '15/01/2015'
 }, {
          'name': 'Puppet Show',
-         'path': 'src/collage1.jpg',
+         'path': 'src/ilustracion5.jpg',
          'date': '05/01/2017'
 }, {
          'name': 'Three Astronauts',
@@ -21,7 +21,7 @@
          'date': '11/12/2016'
 }, {
          'name': 'Split',
-         'path': 'src/collage1.jpg',
+         'path': 'src/ilustracion3.jpg',
          'date': '29/07/2014'
 }, {
          'name': 'Crude Reality',
@@ -29,21 +29,24 @@
          'date': '03/02/2017'
 }, {
          'name': 'No brainer',
-         'path': 'src/collage1.jpg',
+         'path': 'src/ilustracion4.jpg',
          'date': '14/01/2016'
 }]
      $(document).ready(function () {
+         images.forEach(function (a) {
+             $("#gallery").append('<div class="card" style="background-image: url('+a.path+')"> <div class="black"></div> <span><h2>Title:</h2>'+a.name+'<br><h2>Date:</h2>'+a.date+'</span></div>');
+         });
          $('#fullpage').fullpage({
              anchors: ['1P', '2P', '3P', '4P', '5P'],
              normalScrollElements: '#circle, #contact',
-             scrollOverflow:true
+             scrollOverflow: true
          });
          $('#circle').click(function () {
              $(this).toggleClass('active');
              $('#contact').toggleClass('active');
          });
-//         images.forEach(function (a) {
-//             $("#work").append("");
-//         });
+         $(".card").hover(function () {
+             $(this).toggleClass('active');
+         })
      });
  };
